a5cc4698ffa2417a52cbac116297c58ba5c08073
Aman Yadav <amanmanohar011@gmail.com>
2025-05-17 23:58:30 +0530

    First commit!

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import RandomColorGenerator from './components/index.jsx'
+
+function App() {
+
+  return (
+    <>
+      <RandomColorGenerator/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style.css'
+
+export default function RandomColorGenerator() {
+  return (
+    <div className='container'>
+      <button>Create HEX color</button>
+      <button>Create RGB color</button>
+      <button>Generate Random color</button>
+    </div>
+  )
+}
